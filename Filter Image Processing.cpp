6a745278078f1d3514c398918c590ff64d8145cf
Ziad Ismael <117987711+ziadismael@users.noreply.github.com>
2023-10-10 01:59:54 +0300

    Overall Code Improvements. (check description)
    In this commit me and my teammates worked on handling errors and invalid inputs from the user or any sort of malfunction that may cause to crash the program. We also worked on the naming by unifying the names of variables through the same code while following the instructions of naming variables and functions in C++. All to allow the user experience and the code reader to be more clear and concise.

--- Filter Image Processing.cpp
+++ Filter Image Processing.cpp
@@ -1,10 +1,10 @@
 /*
 FCAI – OOP Programming – 2023 - Assignment 1 
 Program: ImageFilterApplier.cpp
-Last Modification Date: 6/10/2023
-Team Members:   1- Mohamed Mahmoud Khamis Rezk               (20221129)
-                2- Ibrahem Medhat Mahmoud Mohamed El Zennary (20221003)
-                3- Ziad Sherif Ahmed Mohamed Ismael          (20220143)
+Last Modification Date: 10/10/2023
+Team Members:   1- Mohamed Mahmoud Khamis Rezk               (20221129)    mohamedkhamis20045@gmail.com
+                2- Ibrahem Medhat Mahmoud Mohamed El Zennary (20221003)    ibra.medhat@gmail.com
+                3- Ziad Sherif Ahmed Mohamed Ismael          (20220143)    ziad.ismael.studymail@gmail.com
 TA:
 Purpose:
 */
@@ -21,11 +21,11 @@ void menuSelect();
 void saveImage();
 void loadImage();
 void filterBlacknWhite();
-void resizeFlip(); 
-void mergeImages();
+void filterFlip(); 
+void filterMerge();
 void filterInvert(); 
-void darklightImage();
-void resizeRotate();   //  <= added in this commit
+void filterDarkenLighten();
+void filterRotate();
 
 int main(){
     cout<<"Ahlan ya user ya habibi"<<endl;
@@ -35,8 +35,9 @@ int main(){
     return 0;
 }
 
+
 void menuSelect(){     //function responsible for handling the choice of the user till he exit    
-    char selector;  //The choice of the user 
+    string selector;  //The choice of the user 
     string options[17]={"1-Black & White Filter","2-Invert Filter",
                         "3-Merge Filter", "4-Flip Image", "5-Darken and Lighten Image",
                         "6-Rotate Image","7-Detect image edges Filter","8-Enlarge Image",   
@@ -47,47 +48,49 @@ void menuSelect(){     //function responsible for handling the choice of the use
     for (size_t i = 0; i < 17; i++){    // printing the menu to the user by the array we made
         cout << options[i] << endl;
     } 
-
-    while (selector != '0'){    // while loop to make sure the use able to choose what to do till he exit by typing 0
+    while (selector[0] != '0'){    // while loop to make sure the user's able to choose what to do till he exit by typing 0
         cout << "Please select a filter to apply , '0' to exit or 's' to save : " ; 
         cin >> selector ;
-        switch (selector){ //Calling the functions by the input of the user
-        case '1':
-            filterBlacknWhite();
-            break;
-        case '2':
-            filterInvert();
-            break;
-        case '3':
-            mergeImages();
-            break;
-        case '4':
-            resizeFlip();
-            break;
-        case '5':
-            darklightImage();
-            break;
-        case '6':
-            resizeRotate();
-            break;
-        case 's':
-            saveImage();
-            break;
-        case '0':
-           break;
-        default: // handling input errors
-            cout<<"Invalid Input or Filter not created yet" << endl;
+        if (selector.length() == 1){
+            switch (selector[0]){ //Calling the functions by the input of the user
+            case '1':
+                filterBlacknWhite();
+                break;
+            case '2':
+                filterInvert();
+                break;
+            case '3':
+                filterMerge();
+                break;
+            case '4':
+                filterFlip();
+                break;
+            case '5':
+                filterDarkenLighten();
+                break;
+            case '6':
+                filterRotate();
+                break;
+            case 's':
+                saveImage();
+                break;
+            case '0':
+                break;
+            default: // handling input errors
+                cout<<"Invalid Input or Filter not created yet" << endl;
+            }
+        }
+        else{
+            cout << "Invalid Input or Filter not created yet" << endl;
         }
     }
 }
-
 void loadImage(){   //responsible for loading the image into 2D array
     char image_name[100];
     cin >> image_name; // after taking image name
     strcat (image_name, ".bmp"); //making sure its the correct format 
     readGSBMP(image_name, image);
 }
-
 void saveImage(){   //responsible for saving the image from 2D array after applying filters
     char image_name[100];
     cout << "Enter the name of the new image : " ;
@@ -96,8 +99,7 @@ void saveImage(){   //responsible for saving the image from 2D array after apply
     writeGSBMP(image_name, image); //function from the "mpblib" library to convert array to image
     cout << "Done! Image has been saved as : " << image_name << endl;
 }
-
-void filterBlacknWhite(){ 
+void filterBlacknWhite(){   
     for (size_t i=0; i<SIZE; i++){
         for (size_t j=0; j<SIZE; j++){ //iterating through each pixel i,j
             if(image[i][j]<=127) // applying the average as a threshold for black
@@ -106,42 +108,52 @@ void filterBlacknWhite(){
                 image[i][j] = 255; // turning any pixel > 127 into white 
         }
     }
+    cout << "Filter has been applied." << endl;
 }
-
-void resizeFlip(){
-    char selector;
+void filterFlip(){
+    string selector;
     char image_copy[SIZE][SIZE];
-    cout<<"Flip (h)orizontally or (v)ertically? ";
-    cin>>selector;
+
     for(size_t i=0; i<SIZE; i++){
         for(size_t j=0; j<SIZE; j++){
             image_copy[i][j] = image[i][j]; //making a copy of the 2D array
         }
-    }
-    if (selector=='h'){
-        for(size_t i=0; i<SIZE/2; i++){ // we only need to go to half of the array cuz if we continue it will revert the flip
-            for(size_t j=0; j<SIZE; j++){
-                int temp = image[i][j];                //  <=
-                image[i][j] = image_copy[SIZE-1-i][j]; //  <=  this three lines of code responsible for swapping pixels
-                image[SIZE-i-1][j] = temp;             //  <=
+    }   
+    cout<<"Flip (h)orizontally or (v)ertically? ";
+    while (true)
+    {
+        cin>>selector;
+        if (selector[0]=='h' && selector.length() == 1){
+            for(size_t i=0; i<SIZE/2; i++){ // we only need to go to half of the array cuz if we continue it will revert the flip
+                for(size_t j=0; j<SIZE; j++){
+                    int temp = image[i][j];                //  <=
+                    image[i][j] = image_copy[SIZE-1-i][j]; //  <=  this three lines of code responsible for swapping pixels
+                    image[SIZE-i-1][j] = temp;             //  <=
+                }
             }
+        cout << "Filter has been applied." << endl;
+        break;// make sure to exit from the while loop if the user entered valid input
+        }
+        else if(selector[0]=='v' && selector.length() == 1){
+            for(size_t i=0; i<SIZE; i++){
+                for(size_t j=0; j<SIZE/2; j++){
+                    int temp = image[i][j];
+                    image[i][j] = image_copy[i][SIZE-1-j];
+                    image[i][SIZE-j-1] = temp;
+                }
+            }
+        cout << "Filter has been applied." << endl;
+        break;// make sure to exit from the while loop if the user entered valid input
+        }
+        else{
+            cout<<"invalid choice. Type (h) to flip horizontally and (v) to flip vertically."<<endl;
         }
     }
-    else if(selector=='v'){
-        for(size_t i=0; i<SIZE; i++){
-            for(size_t j=0; j<SIZE/2; j++){
-                int temp = image[i][j];
-                image[i][j] = image_copy[i][SIZE-1-j];
-                image[i][SIZE-j-1] = temp;
-            }
-        }
-    }
-    else{
-        cout<<"invalid choice. Type (h) to flip horizontally and (v) to flip vertically."<<endl;
-        resizeFlip();  // Handling errors
-    }
+        
+    
+    
 }
-void mergeImages(){
+void filterMerge(){
   //declaring and intializing the name and the array of the second image
   char second_image_name[100];
   unsigned char second_image [SIZE][SIZE];
@@ -165,68 +177,88 @@ void filterInvert(){
             image[i][j]=255-image[i][j];
         }
     }
+    cout << "Filter has been applied." << endl;
 }
-
-void darklightImage(){ //responsible for lighten or darken by 50 %
+void filterDarkenLighten(){ //responsible for lighten or darken by 50 %
   // taking the choice from the user either to darken or lighten
-  int choice;
-  cout << "Choose what you need from the following list :" << endl;
-  cout << "1-Darken" << endl << "2-Lighten" << endl;
-  cin >> choice;
-
-  for (size_t i = 0; i < SIZE; i++) // nested loop for increasing or decreasing the brightness by 50 %
+  string selector;
+  cout << "Do you want to (d)arken or (l)ighten? " << endl;
+  while (true)
   {
-    for (size_t j = 0; j < SIZE; j++)
+    cin >> selector;
+    if (selector.length() == 1 && (selector == "l" || selector == "d"))
     {
-        if (choice == 1)
+        for (size_t i = 0; i < SIZE; i++) // nested loop for increasing or decreasing the brightness by 50 %
         {
-          if((image[i][j] +(image[i][j] * 0.5) )<=0) image[i][j] = 0;
-          else image[i][j] = image[i][j] - (image[i][j] ) * 0.5 ;
+            for (size_t j = 0; j < SIZE; j++)
+            {
+                if (selector == "d")
+                {
+                    if((image[i][j] +(image[i][j] * 0.5) ) <= 0) // keep the image in our range of colors 
+                        image[i][j] = 0;   
+                    else 
+                        image[i][j] = image[i][j] - (image[i][j] ) * 0.5 ;
+                }
+                else if (selector == "l")
+                {
+                    if((image[i][j] +(image[i][j] * 0.5) ) >= 255) // keep the image in our range of colors 
+                        image[i][j] = 255; 
+                    else 
+                        image[i][j] = image[i][j] +(image[i][j] ) * 0.5 ;
+                }
+            }
         }
-        else
-        {
-          if((image[i][j] +(image[i][j] * 0.5) )>=255) image[i][j] = 255;
-          else image[i][j] = image[i][j] +(image[i][j] ) * 0.5 ;
-        }
+        cout << "Filter has been applied." << endl;
+        break; // make sure to exit from the while loop if the user entered valid input
     }
+    else
+        cout << "Invalid choice. Please Type (d) to Darken or (l) to Lighten." << endl;
   }
+    
 }
-void resizeRotate(){
-    int selection;
+void filterRotate(){
+    string selector;
     cout<<"Rotate (90) , (180) , (360) ?"<<endl;
-    cin>>selection;
-    if (selection == 360){
-        return; //returns the same photo
-    }
-    else if (selection == 90){
-        unsigned char rotatedImage[SIZE][SIZE]; //creating a new 2d array to store the new rotated image
-        for (size_t i=0;i<SIZE;i++){
-            for (size_t j=0;j<SIZE;j++){
-                rotatedImage[j][SIZE-1-i]=image[i][j];
-            }
+    while (true)
+    {   
+        cin>>selector;
+        if (selector == "360"){
+            cout << "Filter has been applied." << endl;
+            return; //returns the same photo
         }
-         for (size_t i=0;i<SIZE;i++){
-            for (size_t j=0;j<SIZE;j++){
-                image[i][j]=rotatedImage[i][j]; //storing the new values in the original image
+        else if (selector == "90"){
+            unsigned char image_rotated[SIZE][SIZE]; //creating a new 2d array to store the new rotated image
+            for (size_t i=0;i<SIZE;i++){
+                for (size_t j=0;j<SIZE;j++){
+                    image_rotated[j][SIZE-1-i]=image[i][j];
+                }
             }
-        }
-
-    }
-    else if (selection == 180){
-        char rotated_image[SIZE][SIZE]; //creating a new 2d array to store the new rotated image
-        for (int i=0;i<SIZE;i++){
-            for (int j=0;j<SIZE;j++){
-                rotated_image[SIZE-1-i][SIZE-1-j]=image[i][j]; 
+            for (size_t i=0;i<SIZE;i++){
+                for (size_t j=0;j<SIZE;j++){
+                    image[i][j]=image_rotated[i][j]; //storing the new values in the original image
+                }
             }
+            cout << "Filter has been applied." << endl;
+            break;// make sure to exit from the while loop if the user entered valid input
         }
-        for (int i=0;i<SIZE;i++){
-            for (int j=0;j<SIZE;j++){
-                image[i][j]=rotated_image[i][j];//storing the new values in the original image
+        else if (selector == "180"){
+            unsigned char image_rotated[SIZE][SIZE]; //creating a new 2d array to store the new rotated image
+            for (int i=0;i<SIZE;i++){
+                for (int j=0;j<SIZE;j++){
+                    image_rotated[SIZE-1-i][SIZE-1-j]=image[i][j]; 
+                }
             }
+            for (int i=0;i<SIZE;i++){
+                for (int j=0;j<SIZE;j++){
+                    image[i][j]=image_rotated[i][j];//storing the new values in the original image
+                }
+            }
+            cout << "Filter has been applied." << endl;
+            break;// make sure to exit from the while loop if the user entered valid input
+        }
+        else{
+            cout<<"Invalid Input! Type '90' , '180' or '360' to apply the filter : "<<endl;
         }
     }
-    else{
-        cout<<"Invalid Input!"<<endl;
-        resizeRotate(); //handling input errors
-    }
+    
 }
